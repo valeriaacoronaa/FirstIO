39322cde07d7004ae00dda81228f23e6836f5ca3
valeriaacoronaagit config --global user.email vcorona@mail.csuchico.edu <vcorona@ecc-jaguar.csuchico.edu>
2019-09-04 16:16:35 -0700

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,10 @@
+// Author: Valeria Corona 
 #include <iostream>
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is Valeria. What's your name?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
